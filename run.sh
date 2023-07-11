68d533ee2a63437304d834216c228afe8ecdec85
BryanVe <bryan.ve.bv@gmail.com>
2023-07-11 11:07:11 -0500

    feat: set envs in run.sh file

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
+# Cargamos las variables de entorno
+source ./.env
+
 # Levantar los servicios
 docker-compose up -d --build
 
 # Iniciar el nodo master y los nodos worker enlazandolos al master
-docker exec master-node /opt/spark/sbin/start-master.sh
-docker exec worker1-node /opt/spark/sbin/start-worker.sh spark://master-node:7077
-docker exec worker2-node /opt/spark/sbin/start-worker.sh spark://master-node:7077
-docker exec worker3-node /opt/spark/sbin/start-worker.sh spark://master-node:7077
+docker exec $MASTER_NODE /opt/spark/sbin/start-master.sh
+docker exec $WORKER1_NODE /opt/spark/sbin/start-worker.sh spark://$MASTER_NODE:7077
+docker exec $WORKER2_NODE /opt/spark/sbin/start-worker.sh spark://$MASTER_NODE:7077
+docker exec $WORKER3_NODE /opt/spark/sbin/start-worker.sh spark://$MASTER_NODE:7077
