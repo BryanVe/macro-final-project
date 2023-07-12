5884e44fc8c5ca48d1f2f7bdbacef17909addaed
AnthonyLzq <sluzquinosa@uni.pe>
2023-07-12 04:26:34 -0500

    feat: first attempt to send the salaries through sockets

---
+++ server/src/main/java/macro/socket/Client.java
@@ -0,0 +1,98 @@
+package macro.socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+
+import macro.dbo.Salary;
+import macro.utils.CustomCSVReader;
+
+public class Client {
+  private Socket clientSocket;
+  private PrintWriter out;
+  private BufferedReader in;
+
+  private List<Salary> getSalaries() throws Exception {
+    String workingDir = System.getProperty("user.dir");
+    String pathToCsv = workingDir + "/db/salaries.csv";
+    CustomCSVReader<Salary> reader = new CustomCSVReader<>(pathToCsv);
+
+    try {
+      return reader.read(Salary.class);
+    } catch (Exception e) {
+      System.out.println("Error reading CSV file: " + e.getMessage());
+      throw e;
+    }
+  }
+
+  private boolean startConnection(String ip, int port) {
+    try {
+      clientSocket = new Socket(ip, port);
+      out = new PrintWriter(clientSocket.getOutputStream(), true);
+      in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+      return true;
+    } catch (Exception e) {
+      System.out.println("Error starting the connection: " + e.getMessage());
+
+      return false;
+    }
+  }
+
+  private String sendMessage(Object msg) throws IOException {
+    out.println(msg);
+
+    return in.readLine();
+  }
+
+  private boolean stopConnection() {
+    try {
+      in.close();
+      out.close();
+      clientSocket.close();
+
+      return true;
+    } catch (IOException e) {
+      System.out.println("Error stopping the connection: " + e.getMessage());
+
+      return false;
+    }
+  }
+
+  private String salariesToString(List<Salary> salaries) {
+    Gson gson = new GsonBuilder().create();
+    JsonArray jsonArray = gson.toJsonTree(salaries).getAsJsonArray();
+    String salariesString = jsonArray.toString();
+
+    return salariesString;
+  }
+
+  public static void main(String[] args) {
+    Client client = new Client();
+    boolean clientConnected = client.startConnection("127.0.0.1", 6666);
+
+    if (clientConnected) System.out.println("Client connected to server");
+
+    try {
+      List<Salary> salaries = client.getSalaries();
+      String salariesString = client.salariesToString(salaries);
+      String message = "salaries___" + salariesString;
+      String response = client.sendMessage(message);
+      System.out.println("Server response: " + response);
+    } catch (Exception e) {
+      System.out.println("Error sending message to server: " + e.getMessage());
+      return;
+    } finally {
+      boolean clientDisconnected = client.stopConnection();
+
+      if (clientDisconnected) System.out.println("Client disconnected from server");
+    }
+  }
+}
---
+++ server/src/main/java/macro/socket/Server.java
@@ -0,0 +1,72 @@
+package macro.socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.Objects;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import macro.dbo.Salary;
+
+public class Server {
+  private ServerSocket serverSocket;
+  private Socket clientSocket;
+  private PrintWriter out;
+  private BufferedReader in;
+
+  private List<Salary> readSalaries(String salariesString) {
+    try {
+      ObjectMapper mapper = new ObjectMapper();
+
+      return mapper.readValue(salariesString, new TypeReference<List<Salary>>(){});
+    } catch (Exception e) {
+      System.out.println("Error reading salaries: " + e.getMessage());
+      return null;
+    }
+  }
+
+  private void start(int port) {
+    try {
+      serverSocket = new ServerSocket(port);
+      clientSocket = serverSocket.accept();
+      out = new PrintWriter(clientSocket.getOutputStream(), true);
+      in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      String inputLine;
+
+      while ((inputLine = in.readLine()) != null) {
+        System.out.println(inputLine);
+
+        if (inputLine.contains("salaries")) {
+          List<Salary> salariesList = readSalaries(inputLine.split("___")[1]);
+
+          if (Objects.nonNull(salariesList)) out.println("salaries received!");
+          else out.println("error receiving salaries!");
+        } else if (inputLine.equals("bye")) {
+          out.println("bye");
+          break;
+        } else {
+          out.println("not supported command");
+        }
+      }
+    } catch (IOException e) {
+      System.out.println("Error starting the server: " + e.getMessage());
+    }
+  }
+
+  private void stop() throws IOException {
+    in.close();
+    out.close();
+    clientSocket.close();
+    serverSocket.close();
+  }
+
+  public static void main(String[] args) {
+    new Server().start(6666);
+  }
+}
